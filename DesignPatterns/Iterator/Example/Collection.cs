7d9962110ebcc6e11aed8dc49d7531e6feb85ea2
glima <glima@vitalitecnologia.com.br>
2016-12-23 16:03:32 -0200

    [DEV] Iterator Pattern

---
+++ DesignPatterns/Iterator/Example/Collection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Example
+{
+    /// <summary>
+    /// The 'ConcreteAggregate' class
+    /// </summary>
+    class Collection : IAbstractCollection
+    {
+        private ArrayList _items = new ArrayList();
+
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+
+        // Gets item count
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        // Indexer
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Add(value); }
+        }
+    }
+}
---
+++ DesignPatterns/Iterator/Example/IAbstractIterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Example
+{
+    /// <summary>
+    /// The 'Iterator' interface
+    /// </summary>
+    interface IAbstractIterator
+    {
+        Item First();
+        Item Next();
+        bool IsDone { get; }
+        Item CurrentItem { get; }
+    }
+}
