970172c29820a18bd0a0efadfae8bac90b9888af
glima <glima@vitalitecnologia.com.br>
2017-01-09 17:56:12 -0200

    [DEV] Strategy  Pattern

---
+++ DesignPatterns/Strategy/ConcreteStrategy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort(); // Default is Quicksort
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort(); not-implemented
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.MergeSort(); not-implemented
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+}
